6cb7fa7175336a4d50f719dd490bccb6930fe47a
Masih Sadri <sadri.masih@gmail.com>
2025-11-11 01:40:34 +0330

    Copy build output to docs/dist and add fallback loader

---
+++ docs/dist/chunk-c2df8pra.js
@@ -0,0 +1,5 @@
+import{I as r,ba as o,k as t,n as m}from"./chunk-7jrwwarh.js";var n=t(m(),1),u=t(o(),1),p=n.forwardRef(function({variant:f,...a},i){return u.jsxDEV(r,{ref:i,variant:f??"outlined",...a},void 0,!1,void 0,this)}),s=p;
+export{s as a};
+
+//# debugId=A32DBBB2D56CF5A964756E2164756E21
+//# sourceMappingURL=chunk-c2df8pra.js.map
---
+++ docs/dist/chunk-smw5nn20.js
@@ -0,0 +1,3 @@
+
+//# debugId=88061F979127FA7364756E2164756E21
+//# sourceMappingURL=chunk-smw5nn20.js.map
